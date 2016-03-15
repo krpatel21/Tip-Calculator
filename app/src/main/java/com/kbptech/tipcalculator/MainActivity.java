d3e7ff0e4c663565117866683ea036fd027a61c6
Kaushal Patel <kapatel21@gmail.com>
2016-03-15 04:32:19 -0500

    Change id of gui components. Change to numerical keyboard.

--- app/src/main/java/com/kbptech/tipcalculator/MainActivity.java
+++ app/src/main/java/com/kbptech/tipcalculator/MainActivity.java
@@ -1,31 +1,46 @@
 package com.kbptech.tipcalculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-//test change 2
+
+    private String[] arraySpinner;
+    private EditText subtotalEditText;
+    private EditText tipEditText;
+    private EditText taxRate;
+    private EditText totalEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        subtotalEditText = (EditText) findViewById(R.id.subtotalEditText);
+//        tipEditText = new EditText(this, R.id.tip);
+        taxRate = (EditText) findViewById(R.id.taxRateEditText);
+        totalEditText = (EditText) findViewById(R.id.totalEditText);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        subtotalEditText.setText("0.00");
+        taxRate.setText("0.00");
+        totalEditText.setText("0.00");
+
+
+        this.arraySpinner = new String[] {
+                "1", "2", "3", "4", "5"
+        };
+        Spinner s = (Spinner) findViewById(R.id.tipspinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, arraySpinner);
+        s.setAdapter(adapter);
+
     }
 
     @Override
