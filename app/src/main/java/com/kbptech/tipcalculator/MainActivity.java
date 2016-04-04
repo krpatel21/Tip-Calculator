2f3fbdf7321a8c0d9c010ec8e46e3d973a1c3f86
Kaushal Patel <kapatel21@gmail.com>
2016-04-03 21:58:41 -0500

    Created String resources.

--- app/src/main/java/com/kbptech/tipcalculator/MainActivity.java
+++ app/src/main/java/com/kbptech/tipcalculator/MainActivity.java
@@ -78,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
     private TextWatcher amountEditTextWatcher = new TextWatcher(){
 
         @Override
